95465754b3dcfd73dce78859bdbb6482bdb5112d
panzhixiong <1050098245@qq.com>
2019-05-31 13:38:27 +0800

    NO3

--- src/com/pan/App.java
+++ src/com/pan/App.java
@@ -2,10 +2,12 @@ package com.pan;
 
 import com.pan.instance.No1;
 import com.pan.instance.No2;
+import com.pan.instance.No3;
 
 public class App {
     public static void main(String[] args) {
         //No1.solution();
-        No2.solution();
+        //No2.solution();
+        No3.solution();
     }
 }
